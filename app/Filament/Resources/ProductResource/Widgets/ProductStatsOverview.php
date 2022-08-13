5c05d51eeb80656627f9df25f7900e0872f67719
edisyst <editestar@gmail.com>
2022-08-13 10:41:54 +0200

    DONE: Invoice

---
+++ app/Filament/Resources/ProductResource/Widgets/ProductStatsOverview.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Filament\Resources\ProductResource\Widgets;
+
+use Filament\Widgets\StatsOverviewWidget as BaseWidget;
+use Filament\Widgets\StatsOverviewWidget\Card;
+
+class ProductStatsOverview extends BaseWidget
+{
+    protected function getCards(): array
+    {
+        return [
+            //
+        ];
+    }
+}
